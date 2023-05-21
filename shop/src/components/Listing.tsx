e955ca0072d4b825c023987d404f34fbc2b0c359
svetlana18055 <svetlana18055@yandex.ru>
2023-05-21 19:41:48 +0300

    правки

--- shop/src/components/Listing.tsx
+++ shop/src/components/Listing.tsx
@@ -1,23 +1,15 @@
-/*import Item from "../models";
-import SlicedString from "../models";
-
-interface Props {
-    item: Item,
-    stringTitle: SlicedString
-}*/
+import Item from "../models";
 
 
 export default function Listing ({items}) {
 
-    const sliceTitle = (title) => {
+    const sliceTitle = (title: string) => {
         let limit = 50
-        let slicedString = title.slice( 0, limit)
-        title = slicedString + "..."
-        return title
+        return title.slice( 0, limit) + "..."
     }
 
 
-    const itemElement = (item) => <div className="item" key={item.listing_id}>
+    const itemElement = (item: Item) => <div className="item" key={item.listing_id}>
         <div className="item-image">
             <a href={item.url}>
                 <img src={item.MainImage["url_170x135"]} alt={item.title}/>
--- shop/src/models.tsx
+++ shop/src/models.tsx
@@ -2,13 +2,9 @@ export default interface Item {
     listing_id: number;
     url?: string;
     MainImage: string;
-    title?: string;
+    title: string;
     currency_code: string;
     price?: string;
     quantity?: number;
     level: string;
-}
-
-export default interface SlicedString {
-    StringTitle?: string;
 }
